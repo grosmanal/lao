57a3c0d01b057e925f6e40fc0520ff11f468efc2
Emmanuel Averty <emmanuel.averty@free.fr>
2021-11-25 18:30:25 +0100

    Fix #111 redirection après login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function index(Request $request, LoggerInterface $logger): RedirectResponse
+    #[Route('/', name: 'home')]
+    public function index(): RedirectResponse
     {
-        return $this->redirectToRoute('patient', [ 'id' => 1 ]);
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->redirectToRoute('search');
     }
 }
